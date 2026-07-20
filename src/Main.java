627f396e83d93408760aed4bd354428e6a76b9cb
Wasan <wasanalgharbi@gmail.com>
2026-07-21 00:54:46 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println("            MADA");
+        System.out.println("   Make A Dream Achievable");
+        System.out.println("================================");
+
+        System.out.println();
+        System.out.println("Welcome to MADA!");
+        System.out.println("Build your future, one goal at a time.");
+
+    }
+}
