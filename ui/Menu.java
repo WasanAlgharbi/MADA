237829269a6034a5b437d52de06ad83129e1f26f
Wasan <wasanalgharbi@gmail.com>
2026-07-28 00:10:03 +0300

    Create Menu.java

---
+++ ui/Menu.java
@@ -0,0 +1,21 @@
+package ui;
+
+public class Menu {
+
+    public static void displayMainMenu() {
+
+        System.out.println("\n========== MADA ==========");
+        System.out.println("1. Dashboard");
+        System.out.println("2. Courses");
+        System.out.println("3. Goals");
+        System.out.println("4. Projects");
+        System.out.println("5. Skills");
+        System.out.println("6. Certificates");
+        System.out.println("7. Achievements");
+        System.out.println("8. Career Plan");
+        System.out.println("9. Exit");
+        System.out.print("Choose an option: ");
+
+    }
+
+}
