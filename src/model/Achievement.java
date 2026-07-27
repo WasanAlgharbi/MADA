1dca0a042b2ef1c436c354cfc8cbfd36e9026d8e
Wasan <wasanalgharbi@gmail.com>
2026-07-27 22:13:26 +0300

    Update Achievement.java

--- src/model/Achievement.java
+++ src/model/Achievement.java
@@ -1,49 +1,14 @@
 package model;
 
-import java.time.LocalDate;
-
 public class Achievement {
 
-    private String title;
-    private String provider;
-    private LocalDate date;
-    private String certificatePath;
+    private String achievement;
 
-    public Achievement(
-            String title,
-            String provider,
-            LocalDate date,
-            String certificatePath
-    ) {
-        this.title = title;
-        this.provider = provider;
-        this.date = date;
-        this.certificatePath = certificatePath;
+    public Achievement(String achievement) {
+        this.achievement = achievement;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getProvider() {
-        return provider;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public String getCertificatePath() {
-        return certificatePath;
-    }
-
-    @Override
-    public String toString() {
-
-        return title
-                + " | Provider: "
-                + provider
-                + " | Date: "
-                + date;
+    public String getAchievement() {
+        return achievement;
     }
 }
