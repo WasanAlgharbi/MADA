56fd247aef277be9f44c4259d58a2abf2845d2a4
Wasan <wasanalgharbi@gmail.com>
2026-07-27 22:10:25 +0300

    Update Project.java

--- src/model/Project.java
+++ src/model/Project.java
@@ -2,57 +2,19 @@ package model;
 
 public class Project {
 
-    private String name;
+    private String title;
     private String description;
-    private String technologies;
-    private boolean completed;
 
-    public Project(
-            String name,
-            String description,
-            String technologies
-    ) {
-        this.name = name;
+    public Project(String title, String description) {
+        this.title = title;
         this.description = description;
-        this.technologies = technologies;
-        this.completed = false;
     }
 
-    public void complete() {
-        completed = true;
-    }
-
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
     public String getDescription() {
         return description;
-    }
-
-    public String getTechnologies() {
-        return technologies;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    @Override
-    public String toString() {
-
-        String status;
-
-        if (completed) {
-            status = "Completed";
-        } else {
-            status = "In Progress";
-        }
-
-        return name
-                + " | "
-                + technologies
-                + " | "
-                + status;
     }
 }
