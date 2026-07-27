fdfb1c1d89b08fe302f6cefbc7dd9b9e828f6db7
Wasan <wasanalgharbi@gmail.com>
2026-07-27 22:07:54 +0300

    Goal.java

--- src/model/Goal.java
+++ src/model/Goal.java
@@ -1,138 +1,24 @@
 package model;
 
-import java.util.ArrayList;
-
 public class Goal {
 
-    private String name;
+    private String title;
+    private boolean completed;
 
-    private ArrayList<Milestone>
-            milestones;
-
-    private ArrayList<String>
-            completedDates;
-
-    public Goal(String name) {
-
-        this.name = name;
-
-        milestones =
-                new ArrayList<>();
-
-        completedDates =
-                new ArrayList<>();
+    public Goal(String title) {
+        this.title = title;
+        this.completed = false;
     }
 
-    public void addMilestone(
-            String milestoneName
-    ) {
-
-        Milestone milestone =
-                new Milestone(
-                        milestoneName
-                );
-
-        milestones.add(milestone);
-    }
-
-    public void completeMilestone(
-            int index
-    ) {
-
-        if (
-                index >= 0
-                        && index < milestones.size()
-        ) {
-            milestones
-                    .get(index)
-                    .complete();
-        }
-    }
-
-    public void recordProgress(
-            String date
-    ) {
-
-        if (
-                !completedDates
-                        .contains(date)
-        ) {
-            completedDates.add(date);
-        }
-    }
-
-    public double calculateProgress() {
-
-        if (milestones.isEmpty()) {
-            return 0;
-        }
-
-        int completed = 0;
-
-        for (
-                Milestone milestone
-                : milestones
-        ) {
-            if (
-                    milestone.isCompleted()
-            ) {
-                completed++;
-            }
-        }
-
-        return (
-                completed * 100.0
-        ) / milestones.size();
+    public String getTitle() {
+        return title;
     }
 
     public boolean isCompleted() {
-
-        return calculateProgress()
-                == 100;
+        return completed;
     }
 
-    public void displayGoal() {
-
-        System.out.println(
-                "\nGoal: "
-                        + name
-        );
-
-        System.out.println(
-                "Progress: "
-                        + calculateProgress()
-                        + "%"
-        );
-
-        System.out.println(
-                "\nMilestones:"
-        );
-
-        for (
-                int i = 0;
-                i < milestones.size();
-                i++
-        ) {
-            System.out.println(
-                    (i + 1)
-                            + ". "
-                            + milestones.get(i)
-            );
-        }
-
-        System.out.println(
-                "\nCompleted sessions: "
-                        + completedDates.size()
-        );
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public ArrayList<Milestone>
-    getMilestones() {
-
-        return milestones;
+    public void completeGoal() {
+        completed = true;
     }
 }
