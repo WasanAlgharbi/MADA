faa7aa13255c5812b7314a30590b23436bc2e52f
Wasan <wasanalgharbi@gmail.com>
2026-07-27 22:14:28 +0300

    Update CareerPlan.java

--- src/model/CareerPlan.java
+++ src/model/CareerPlan.java
@@ -1,63 +1,20 @@
 package model;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+public class CareerPlan {
 
-public class StudySession {
+    private String careerGoal;
+    private String targetCompany;
 
-    private LocalDate date;
-    private LocalTime startTime;
-    private int durationMinutes;
-    private boolean completed;
-
-    public StudySession(
-            LocalDate date,
-            LocalTime startTime,
-            int durationMinutes
-    ) {
-        this.date = date;
-        this.startTime = startTime;
-        this.durationMinutes = durationMinutes;
-        this.completed = false;
+    public CareerPlan(String careerGoal, String targetCompany) {
+        this.careerGoal = careerGoal;
+        this.targetCompany = targetCompany;
     }
 
-    public void complete() {
-        completed = true;
+    public String getCareerGoal() {
+        return careerGoal;
     }
 
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public int getDurationMinutes() {
-        return durationMinutes;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    @Override
-    public String toString() {
-
-        String status;
-
-        if (completed) {
-            status = "Completed";
-        } else {
-            status = "Not completed";
-        }
-
-        return "Date: " + date
-                + " | Time: " + startTime
-                + " | Duration: "
-                + durationMinutes
-                + " minutes"
-                + " | Status: "
-                + status;
+    public String getTargetCompany() {
+        return targetCompany;
     }
 }
