0901007ab3fd4a05fedf6d331f61608a062d6fa2
Wasan <wasanalgharbi@gmail.com>
2026-07-28 04:52:29 +0300

    Update Project.java

--- src/model/Project.java
+++ src/model/Project.java
@@ -4,11 +4,21 @@ public class Project {
 
     private String title;
     private String description;
+    private String technology;
+    private String status;
 
-    public Project(String title, String description) {
+    public Project(String title, String description, String technology, String status) {
+
         this.title = title;
         this.description = description;
+        this.technology = technology;
+        this.status = status;
+
     }
+
+    // ===========================
+    // Getters
+    // ===========================
 
     public String getTitle() {
         return title;
@@ -16,5 +26,31 @@ public class Project {
 
     public String getDescription() {
         return description;
+    }
+
+    public String getTechnology() {
+        return technology;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // ===========================
+    // Setters
+    // ===========================
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+
+        return title +
+               "\nTechnology: " + technology +
+               "\nStatus: " + status +
+               "\nDescription: " + description;
+
     }
 }
