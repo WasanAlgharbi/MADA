2c4600adc47e2b2061db9b457ae8b38c82e01f8c
Wasan <wasanalgharbi@gmail.com>
2026-07-28 04:53:28 +0300

    Update Skill.java

--- src/model/Skill.java
+++ src/model/Skill.java
@@ -3,19 +3,48 @@ package model;
 public class Skill {
 
     private String skillName;
+    private String category;
     private String level;
 
-    public Skill(String skillName, String level) {
+    public Skill(String skillName, String category, String level) {
+
         this.skillName = skillName;
+        this.category = category;
         this.level = level;
+
     }
+
+    // ===========================
+    // Getters
+    // ===========================
 
     public String getSkillName() {
         return skillName;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
     public String getLevel() {
         return level;
     }
-}
 
+    // ===========================
+    // Setters
+    // ===========================
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+
+        return skillName +
+               "\nCategory: " + category +
+               "\nLevel: " + level;
+
+    }
+
+}
