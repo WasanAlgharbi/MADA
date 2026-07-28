a61f6b81e9436e88d88f91b15d1d81c60f5ca959
Wasan <wasanalgharbi@gmail.com>
2026-07-28 04:54:16 +0300

    Update Achievement.java

--- src/model/Achievement.java
+++ src/model/Achievement.java
@@ -2,13 +2,37 @@ package model;
 
 public class Achievement {
 
-    private String achievement;
+    private String title;
+    private String description;
+    private String date;
 
-    public Achievement(String achievement) {
-        this.achievement = achievement;
+    public Achievement(String title, String description, String date) {
+
+        this.title = title;
+        this.description = description;
+        this.date = date;
+
     }
 
-    public String getAchievement() {
-        return achievement;
+    public String getTitle() {
+        return title;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Achievement: " + title +
+                "\nDescription: " + description +
+                "\nDate: " + date;
+
+    }
+
 }
