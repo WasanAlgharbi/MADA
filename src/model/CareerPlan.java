32843fd18c28d359d07c5c943a552fbb5f71f293
Wasan <wasanalgharbi@gmail.com>
2026-09-11 18:21:32 +0300

    Update CareerPlan.java

--- src/model/CareerPlan.java
+++ src/model/CareerPlan.java
@@ -1,40 +1,62 @@
 package model;
 
-public class CareerPlan {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CareerPlan implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private String careerGoal;
-    private String targetCompany;
-    private String requiredSkills;
+    private List<String> requiredSkills;
+    private List<String> requiredCertificates;
+    private List<String> targetCompanies;
 
-    public CareerPlan(String careerGoal,
-                      String targetCompany,
-                      String requiredSkills) {
-
+    public CareerPlan(String careerGoal) {
         this.careerGoal = careerGoal;
-        this.targetCompany = targetCompany;
-        this.requiredSkills = requiredSkills;
-
+        this.requiredSkills = new ArrayList<>();
+        this.requiredCertificates = new ArrayList<>();
+        this.targetCompanies = new ArrayList<>();
     }
 
     public String getCareerGoal() {
         return careerGoal;
     }
 
-    public String getTargetCompany() {
-        return targetCompany;
+    public List<String> getRequiredSkills() {
+        return requiredSkills;
     }
 
-    public String getRequiredSkills() {
-        return requiredSkills;
+    public List<String> getRequiredCertificates() {
+        return requiredCertificates;
+    }
+
+    public List<String> getTargetCompanies() {
+        return targetCompanies;
+    }
+
+    public void setCareerGoal(String careerGoal) {
+        this.careerGoal = careerGoal;
+    }
+
+    public void addRequiredSkill(String skill) {
+        requiredSkills.add(skill);
+    }
+
+    public void addRequiredCertificate(String certificate) {
+        requiredCertificates.add(certificate);
+    }
+
+    public void addTargetCompany(String company) {
+        targetCompanies.add(company);
     }
 
     @Override
     public String toString() {
-
-        return "Career Goal: " + careerGoal +
-                "\nTarget Company: " + targetCompany +
-                "\nRequired Skills: " + requiredSkills;
-
+        return "Career Goal: " + careerGoal
+                + "\nRequired Skills: " + requiredSkills
+                + "\nRequired Certificates: " + requiredCertificates
+                + "\nTarget Companies: " + targetCompanies;
     }
-
 }
