aeb5ee94743ec7f28fecf5f9cfa63957d824095a
Wasan <wasanalgharbi@gmail.com>
2026-09-11 18:20:29 +0300

    Update Certificate.java

--- src/model/Certificate.java
+++ src/model/Certificate.java
@@ -1,20 +1,55 @@
 package model;
 
-public class Certificate {
+import java.io.Serializable;
 
-    private String title;
-    private String organization;
+public class Certificate implements Serializable {
 
-    public Certificate(String title, String organization) {
-        this.title = title;
-        this.organization = organization;
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private String issuer;
+    private String date;
+
+    public Certificate(
+            String name,
+            String issuer,
+            String date
+    ) {
+        this.name = name;
+        this.issuer = issuer;
+        this.date = date;
     }
 
-    public String getTitle() {
-        return title;
+    public String getName() {
+        return name;
     }
 
-    public String getOrganization() {
-        return organization;
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return name
+                + " | Issuer: "
+                + issuer
+                + " | Date: "
+                + date;
     }
 }
