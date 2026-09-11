a489374ef02477d0cb86c41478485933dc9ef6be
Wasan <wasanalgharbi@gmail.com>
2026-09-11 18:24:22 +0300

    Update CareerService.java

--- src/service/CareerService.java
+++ src/service/CareerService.java
@@ -1,13 +1,74 @@
 package service;
 
 import model.CareerPlan;
+import model.Student;
 
 public class CareerService {
 
-    public void displayCareer(CareerPlan career) {
+    private final Student student;
 
-        System.out.println("Career Goal: " + career.getCareerGoal());
-        System.out.println("Target Company: " + career.getTargetCompany());
+    public CareerService(Student student) {
+        this.student = student;
+    }
 
+    public Student getStudent() {
+        return student;
+    }
+
+    public void createOrUpdatePlan(String goal) {
+
+        if (student.getCareerPlan() == null) {
+
+            student.setCareerPlan(
+                    new CareerPlan(goal)
+            );
+
+        } else {
+
+            student.getCareerPlan()
+                    .setCareerGoal(goal);
+        }
+    }
+
+    public CareerPlan getCareerPlan() {
+        return student.getCareerPlan();
+    }
+
+    public boolean addRequiredSkill(String skill) {
+
+        if (student.getCareerPlan() == null) {
+            return false;
+        }
+
+        student.getCareerPlan()
+                .addRequiredSkill(skill);
+
+        return true;
+    }
+
+    public boolean addRequiredCertificate(
+            String certificate
+    ) {
+
+        if (student.getCareerPlan() == null) {
+            return false;
+        }
+
+        student.getCareerPlan()
+                .addRequiredCertificate(certificate);
+
+        return true;
+    }
+
+    public boolean addTargetCompany(String company) {
+
+        if (student.getCareerPlan() == null) {
+            return false;
+        }
+
+        student.getCareerPlan()
+                .addTargetCompany(company);
+
+        return true;
     }
 }
